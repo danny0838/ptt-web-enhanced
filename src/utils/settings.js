7f1510bd7af610509d038b9f12406627a5fbc75a
Danny Lin <danny0838@gmail.com>
2017-09-25 01:12:07 +0800

    優化 pweSettings
    - storageArea.get 可直接傳入物件指定要擷取的 key 及其預設值
    - 使用 return await 使前後幾個 method 規格一致，使語意更明確，且使 Promise 萬一發生錯誤會是在函數內出錯而非函數外

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -11,21 +11,17 @@ pweSettings.defaultSettings = {
 };
 
 pweSettings.get = async function(key) {
-    const settings = await browser.storage.sync.get(key);
-    return (settings[key] !== undefined) ? settings[key] : defaultSettings[key];
+    return (await browser.storage.sync.get({[key]: this.defaultSettings[key]}))[key];
 };
 
 pweSettings.getAll = async function() {
-    const settings = await browser.storage.sync.get();
-    return Object.assign({}, pweSettings.defaultSettings, settings);
+    return await browser.storage.sync.get(this.defaultSettings);
 };
 
 pweSettings.set = async function(key, value) {
-    return browser.storage.sync.set({
-        [key]: value
-    });
+    return await browser.storage.sync.set({[key]: value});
 };
 
 pweSettings.reset = async function() {
-    return browser.storage.sync.clear();
+    return await browser.storage.sync.clear();
 };
